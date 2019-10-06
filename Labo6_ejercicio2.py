c879768a9f9e9080f02952b53647392532378ace
Andre <00103017@uca.edu.sv>
2019-10-05 18:03:11 -0600

    validado

--- Labo6_ejercicio2.py
+++ Labo6_ejercicio2.py
@@ -3,8 +3,13 @@ número de filas ingresado. Validaciones las entradas de la manera que
 considere necesaria.
 """
 
+flag = 0
+while(flag == 0):
+    try:
+        num = int (input("Ingresa la cantidad de filas: "))
+        for i in range (1, num+1):
+            print(str(str(str(i)+" ")*i))
+        flag = 1   
+    except ValueError:
+        print("Tienes que meter un número, intentalo de nuevo")
 
-num = int(input("Ingresa la cantidad de filas: "))
-
-for i in range (1, num+1):
-    print(str(str(i)*i))
